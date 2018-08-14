87d20961fd85d8ce17d4d868e1a826fabb38d19a
mimigood <mbgss07@gmail.com>
2018-08-14 19:06:29 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,14 +1,16 @@
 require_relative 'config/environment'
 
-get '/name' do
-  "My name is Mimi"
-end
-
-get '/hometown' do
-  "Western Springs"
-end 
-
-get '/fav'
-
 class App < Sinatra::Base
+
+  get '/name' do 
+    "My name is Mimi"
+  end
+
+  get '/hometown' do 
+    "My hometown is Western Springs"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Oh, Jeez"
+  end
 end
